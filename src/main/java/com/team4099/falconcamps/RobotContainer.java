6ba5515b40d835324557b98ad0a0a85cab69fed5
juanixco0192 <ixcj8573@chsphantoms.org>
2020-07-16 17:53:30 +0000

    .

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,6 +1,7 @@
 package com.team4099.falconcamps;
 
 import com.team4099.falconcamps.commands.ShootCommand;
+import com.team4099.falconcamps.commands.ShooterIdleCommand;
 import com.team4099.falconcamps.subsystems.Drivetrain;
 import com.team4099.falconcamps.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -10,11 +11,12 @@ public class RobotContainer {
     public Shooter shooter = new Shooter();
 
     public RobotContainer() {
+        shooter.setDefaultCommand(new ShooterIdleCommand(shooter));
 
     }
 
     public Command getAutonomousCommand() {
-        ShootCommand shoot = new ShootCommand(shooter)
+        ShootCommand shoot = new ShootCommand(shooter);
         return shoot.withTimeout(5);
     }
 }
